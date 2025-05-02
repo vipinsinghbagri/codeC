7e9f03ef6a9ad323fb4258eb80c6fa4e56e6acc5
vipinsinghbagri <155605302+vipinsinghbagri@users.noreply.github.com>
2025-05-02 11:56:15 +0530

    Create EvenOdd3.c

---
+++ EvenOdd3.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+void main(){
+    int a;
+    printf("Enter a Number : ");
+    scanf("%d",&a);
+    printf("%d",a);
+    if(a%2==0)
+    printf(" is Even"); //1 True
+    else
+     printf(" is Odd");  //0 False
+}
